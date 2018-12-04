16cef54a701bcfb9aadb8d4d41a7b4e8ff17f600
Jingjing Duan <duanjingjing@gmail.com>
2018-12-04 10:15:05 -0800

    2nd part

--- src/main/kotlin/year2018/Day3.kt
+++ src/main/kotlin/year2018/Day3.kt
@@ -3,6 +3,7 @@ package year2018
 import java.io.File
 
 data class Rectangle(
+    val id: Int,
     val x: Int,
     val y: Int,
     val width: Int,
@@ -24,18 +25,7 @@ data class Rectangle(
 }
 
 fun findSquareInches(path: String): Int {
-  val coveredTimes = mutableMapOf<Pair<Int, Int>, Int>()
-  File(path).forEachLine {
-    val rectangle = parseLine(it)
-    val pairs = rectangle.coveredCells()
-    for (pair in pairs) {
-      if (pair in coveredTimes) {
-        coveredTimes[pair] = coveredTimes[pair]!! + 1
-      } else {
-        coveredTimes[pair] = 1
-      }
-    }
-  }
+  val coveredTimes = findAllCoveredSquares(path)
 
   var squareInches = 0
   for (pair in coveredTimes.keys) {
@@ -47,13 +37,55 @@ fun findSquareInches(path: String): Int {
   return squareInches
 }
 
+// part 2
+fun findSquareId(path: String): Int {
+  val coveredTimes = findAllCoveredSquares(path)
+
+  val lines = File(path).readLines()
+  for (i in 0 until lines.size) {
+    val rectangle = parseLine(lines[i])
+    val pairs = rectangle.coveredCells()
+    var flag = true
+    for (pair in pairs) {
+      if (coveredTimes[pair]!! > 1) {
+        flag = false
+        break
+      }
+    }
+
+    if (flag) {
+      return rectangle.id
+    }
+  }
+
+  throw Exception("Not found!")
+}
+
+fun findAllCoveredSquares(path: String): Map<Pair<Int, Int>, Int> {
+  val coveredTimes = mutableMapOf<Pair<Int, Int>, Int>()
+  File(path).forEachLine {
+    val rectangle = parseLine(it)
+    val pairs = rectangle.coveredCells()
+
+    for (pair in pairs) {
+      if (pair in coveredTimes) {
+        coveredTimes[pair] = coveredTimes[pair]!! + 1
+      } else {
+        coveredTimes[pair] = 1
+      }
+    }
+  }
+
+  return coveredTimes
+}
+
 /**
  * Parse a string like:
  * #16 @ 859,624: 21x19
  * and return a Rectangle object.
  */
 fun parseLine(line: String): Rectangle {
-  val re = """#\d+\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)""".toRegex()
+  val re = """#(\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)""".toRegex()
   val parts = re.find(line)!!.groupValues
-  return Rectangle(parts[1].toInt(), parts[2].toInt(), parts[3].toInt(), parts[4].toInt())
+  return Rectangle(parts[1].toInt(), parts[2].toInt(), parts[3].toInt(), parts[4].toInt(), parts[5].toInt())
 }
--- src/test/kotlin/year2018/Day3Test.kt
+++ src/test/kotlin/year2018/Day3Test.kt
@@ -5,8 +5,8 @@ import org.junit.Test
 
 class Day3Test {
   @Test fun test1() {
-    assertEquals(Rectangle(859, 624, 21, 19), parseLine("#16 @ 859,624: 21x19"))
-    assertEquals(Rectangle(754, 921, 21, 15), parseLine("#155 @ 754,921: 21x15"))
+    assertEquals(Rectangle(16, 859, 624, 21, 19), parseLine("#16 @ 859,624: 21x19"))
+    assertEquals(Rectangle(155, 754, 921, 21, 15), parseLine("#155 @ 754,921: 21x15"))
   }
 
   @Test fun test2() {
@@ -18,11 +18,16 @@ class Day3Test {
         Pair(2, 0),
         Pair(2, 1)
     )
-    assertEquals(expected, Rectangle(0, 0, 3, 2).coveredCells())
+    assertEquals(expected, Rectangle(1, 0, 0, 3, 2).coveredCells())
   }
 
   @Test fun test3() {
     val inputFile = System.getProperty("user.dir") + "/src/test/kotlin/year2018/Day3Input.txt"
     assertEquals(98005, findSquareInches(inputFile))
+  }
+
+  @Test fun test4() {
+    val inputFile = System.getProperty("user.dir") + "/src/test/kotlin/year2018/Day3Input.txt"
+    assertEquals(331, findSquareId(inputFile))
   }
 }
